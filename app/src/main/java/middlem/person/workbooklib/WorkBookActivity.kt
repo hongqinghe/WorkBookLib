4f675e24d819e09ab93cf73edc55853f69a6a0ac
gongtong <gongtong@2dfire.com>
2017-10-30 23:17:53 +0800

    init

---
+++ app/src/main/java/middlem/person/workbooklib/WorkBookActivity.kt
@@ -0,0 +1,12 @@
+package middlem.person.workbooklib
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkBookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_work_book)
+    }
+}
